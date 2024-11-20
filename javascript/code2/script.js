a2493c1b591134efa8a3a17d6c9638958adb14ee
Ajay kumar <ajaynanda0001@gmail.com>
2024-11-20 19:31:55 +0530

    initial commit

---
+++ javascript/code2/script.js
@@ -0,0 +1,20 @@
+// let dayy= new Date()
+
+// function start(){
+//    let dt= dayy.getDate();
+//    console.log("Today is",dt)
+
+//    let mon =dayy.getUTCMonth();
+//    console.log("This Mont is",mon)
+// }
+
+var a = "ajay"
+ var b = 20;e = 2; f = b + e;
+      c = 5.59
+ 
+
+console.log(typeof(d))
+
+console.log(Math.pow(b,2))
+console.log(Math.floor(c))
+console.log(Math.ceil(c))
---
+++ javascript/code3/script.js
@@ -0,0 +1,18 @@
+const ele = document.getElementById('head');
+
+function fun()
+{
+    if(ele.textContent == "Good")
+    {
+        ele.textContent="bad";
+        ele.style.color= "orange";
+    }
+    else
+    {
+        ele.textContent="Good";
+        ele.style.color="green";
+    }
+}
+
+
+ let y = 7.6;
